30bbdb39f6e77f617a978d98553e01c1de815ff8
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-18 21:40:41 -0300

    Adding Javascript to a new file: scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+Modal = {
+    open() {
+        // Abrir modal
+        // Adicionar classe active ao modal
+        document.querySelector('.modal-overlay').classList.add('active');
+    },
+    close() {
+        // Fechar modal
+        // Remover classe active do modal
+        document.querySelector('.modal-overlay').classList.remove('active')
+    }
+}
