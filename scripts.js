b3642204e7cbd7c8c023f0d7a03679383bdae773
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-28 11:05:35 -0300

    Removing comments

--- scripts.js
+++ scripts.js
@@ -1,12 +1,8 @@
 Modal = {
     open() {
-        // Open modal
-        // Add active class to modal
         document.querySelector('.modal-overlay').classList.add('active');
     },
     close() {
-        // Close modal
-        // Remove active class from modal
         document.querySelector('.modal-overlay').classList.remove('active')
     }
 }
